11c359e2b55d7c8940f877d535cd5d90a3a3210d
Kaarthicksaran <108985643+Kaarthicksaran@users.noreply.github.com>
2022-08-10 21:03:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let array1 = ["yes", "no", "maybe", "always", "sometimes", "never", "if"];
+
+function reverseArray(arr) {
+  var newArray = [];
+  for (var i = arr.length - 1; i >= 0; i--) {
+    newArray.push(arr[i]);
+  }
+  return newArray;
+}
+
+reverseArray(array1);
